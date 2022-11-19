9b8fe30a5366dbc8b3a53451e5a9071f56aa176e
zoymanukian <zoya.manukyan.05@gmail.com>
2022-11-19 20:08:46 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+
+
+app.use(express.static("."));
+
+app.get('/', function (req, res) {
+
+    res.redirect('index.html');
+
+});
+
+server.listen(3000);
